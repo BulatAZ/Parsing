2f2f4c3502943e05d442823ef94520ce99015fc0
Bulat <zambulat@yandex.ru>
2019-06-11 21:58:31 +0300

    First commit. Start of development

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        /*Разработать консольное приложение которое выполняет функции: 
+    1.	Запрашивает у пользователя адрес страницы сайта
+    2.	Скачивает исходный html этой страницы
+    3.	Получает значения атрибута HREF первых 20  тегов A
+    4.	Выводит в консоль полученные значения
+    5.	Сохраняет в текстовый файл полученные значения*/
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите адрес страницы сайта:");
+            string siteAddress = Console.ReadLine();
+
+            Console.Read();
+        }
+    }
+}
